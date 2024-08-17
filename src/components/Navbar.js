809fb77d37a24b0a9df6038d49fcf8be78cab7c7
Nahc219 <thutrang2220047@gmail.com>
2024-08-17 17:34:58 +0900

    click on Logo for going to Home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,7 +38,12 @@ function NavBar() {
       className={navColour ? 'sticky' : 'navbar'}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex" style={{ flex: '4' }}>
+        <Navbar.Brand
+          as={Link}
+          to="/Chan-Portfolio/"
+          className="d-flex"
+          style={{ flex: '4' }}
+        >
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
